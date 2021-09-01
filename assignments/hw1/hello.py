35c035353fb6c7cefd532306aa8b5b6f58d8e91d
Derek Bolch <bolchdp@g.cofc.edu>
2021-09-01 11:09:36 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Derek Bolch
+hello.py
+
+Problem: this program illustrates a simple print function
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+
+
+main()
