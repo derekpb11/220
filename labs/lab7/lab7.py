61be7c231920519988cd93b2c37ca9daaee747e3
Derek Bolch <bolchdp@g.cofc.edu>
2021-10-13 14:20:00 -0400

    completed lab 7

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -1,13 +1,79 @@
 """
-Name: <your name goes here – first and last>
+Name: Derek Bolch
 Partner: <your partner's name goes here – first and last>
-<ProgramName>.py
+lab7.py
 """
+
+
+def cash_conversion():
+    intake = eval(input("Please enter an integer: "))
+    print('${}.00'.format(intake))
+
+
+def encode():
+    message_input = input("Please enter your message: ")
+    shift = eval(input("Please enter your key: "))
+    final = ''
+    for i in range(len(message_input)):
+        temp_char = message_input[i]
+        temp_ascii = ord(temp_char)
+        if temp_ascii >= 122:
+            temp_ascii = (temp_ascii + shift) - 26
+        elif temp_ascii < 122:
+            temp_ascii = (temp_ascii + shift)
+            temp_char = chr(temp_ascii)
+        final = final + temp_char
+    print(final)
+
+
+def sphere_area(radius):
+    surface_area = 4 * 3.14 * (radius ** 2)
+    return surface_area
+
+
+def sphere_volume(radius):
+    volume = (4 / 3) * 3.14 * (radius ** 3)
+    return volume
+
+
+def sum_n(n):
+    acc = 0
+    for i in range(n):
+        acc = acc + i
+    acc += n
+    return acc
+
+
+def sum_n_cubes(n):
+    acc = 0
+    for i in range(n):
+        acc = acc + (i ** 3)
+    acc += (n ** 3)
+    return acc
+
+
+def encode_better():
+    message = input('Please enter the message you want to encode: ')
+    key = input('Please enter the key: ')
+    final = ''
+    for i in range(len(message)):
+        temp_ord_message = ord(message[i])
+        shift = ord(key[i]) - 97
+        temp_ord_message += shift
+        cipher_message = chr(temp_ord_message)
+        final += cipher_message
+    print(final)
 
 
 def main():
     # add function calls here
-    pass
+    # cash_conversion()
+    # encode()
+    # sphere_area()
+    # sphere_volume()
+    # sum_n()
+    # sum_n_cubes()
+    # encode_better()
 
 
 main()
