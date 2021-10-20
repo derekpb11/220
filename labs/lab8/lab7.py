de136d7ace356d25256a46641332608b8aab8b4b
Derek Bolch <bolchdp@g.cofc.edu>
2021-10-20 14:47:47 -0400

    completed lab 8

---
+++ labs/lab8/lab7.py
@@ -0,0 +1,75 @@
+"""
+Name: Derek Bolch
+Partner: <your partner's name goes here – first and last>
+lab7.py
+"""
+
+
+def cash_conversion():
+    intake = eval(input("Please enter an integer: "))
+    print('${}.00'.format(intake))
+
+
+def encode(message_input, shift):
+    final = ''
+    for i in range(len(message_input)):
+        temp_char = message_input[i]
+        temp_ascii = ord(temp_char)
+        if temp_ascii >= 122:
+            temp_ascii = (temp_ascii + shift) - 26
+        elif temp_ascii < 122:
+            temp_ascii = (temp_ascii + shift)
+            temp_char = chr(temp_ascii)
+        final = final + temp_char
+    return final
+
+
+def sphere_area(radius):
+    surface_area = 4 * 3.14 * (radius ** 2)
+    return surface_area
+
+
+def sphere_volume(radius):
+    volume = (4 / 3) * 3.14 * (radius ** 3)
+    return volume
+
+
+def sum_n(n):
+    acc = 0
+    for i in range(n):
+        acc = acc + i
+    acc += n
+    return acc
+
+
+def sum_n_cubes(n):
+    acc = 0
+    for i in range(n):
+        acc = acc + (i ** 3)
+    acc += (n ** 3)
+    return acc
+
+
+def encode_better(message, key):
+    final = ''
+    for i in range(len(message)):
+        temp_ord_message = ord(message[i])
+        shift = ord(key[i]) - 97
+        temp_ord_message += shift
+        cipher_message = chr(temp_ord_message)
+        final += cipher_message
+    print(final)
+
+
+def main():
+    # add function calls here
+    # cash_conversion()
+    # encode()
+    # sphere_area()
+    # sphere_volume()
+    # sum_n()
+    # sum_n_cubes()
+    # encode_better()
+    pass
+
+main()
--- labs/lab8/lab8.py
+++ labs/lab8/lab8.py
@@ -1,11 +1,89 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Derek Bolch
+lab8.py
 """
+from lab7 import encode, encode_better
+
+
+def number_words(in_file_name, out_file_name):
+    in_file = open(in_file_name, 'r')
+    out_file = open(out_file_name, 'w')
+
+    i = 1
+    for line in in_file:
+        new_line = line.split(' ')
+        for word in new_line:
+            out_file.write(str(i) + " " + word + "\n")
+            i += 1
+    in_file.close()
+    out_file.close()
+
+
+def hourly_wages(in_file_name, out_file_name):
+    in_file = open(in_file_name, 'r')
+    out_file = open(out_file_name, 'w')
+
+    for line in in_file:
+        new_line = line.split(' ')
+        new_line[2] = str(float(new_line[2]) + 1.65)
+        out_file.write(" ".join(new_line) + "\n")
+    in_file.close()
+    out_file.close()
+
+
+def calc_check_sum(isbn):
+    isbn = str(isbn)
+    total = 0
+    for i in range(len(isbn)):
+        num = int(isbn[i]) * (10-i)
+        total = total + num
+    return total % 11
+
+
+def send_message(file, friend_name):
+    in_file = open(file, 'r')
+    out_file = open(friend_name + '.txt', 'w')
+
+    for line in in_file:
+        out_file.write(line + '\n')
+    in_file.close()
+    out_file.close()
+
+
+def send_safe_message(file, friend_name, key):
+    in_file = open(file, 'r')
+    out_file = open(friend_name + '.txt', 'w')
+
+    for line in in_file:
+        new_line = encode(line, key)
+        out_file.write(new_line + '\n')
+
+    in_file.close()
+    out_file.close()
+
+
+def send_uncrackable_message(file, friend_name, pad):
+    in_file = open(file, 'r')
+    pad_file = open(pad, 'r')
+    out_file = open(friend_name + '.txt', 'w')
+
+    pad = pad_file.read()
+    for line in in_file:
+        new_line = encode_better(line, pad)
+        out_file.write(new_line + '\n')
+
+    in_file.close()
+    out_file.close()
+
 
 def main():
     # add other function calls here
-    pass
+    number_words('input.txt', 'output.txt')
+    hourly_wages('hourly_wages.txt', 'output.txt')
+    calc_check_sum('0072946520')
+    send_message('jimmy.txt', 'thomas')
+    send_safe_message('jimmy.txt', 'thomas', 4)
+    send_uncrackable_message('jimmy.txt', 'thomas', 'pad.txt')
 
 
 main()
