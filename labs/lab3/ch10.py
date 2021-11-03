8d1a5142040d8351bcf601da5485cfc0471489b1
Derek Bolch <bolchdp@g.cofc.edu>
2021-11-03 15:02:25 -0400

    completed lab 10

---
+++ labs/lab3/ch10.py
@@ -0,0 +1,12 @@
+"""
+Name: Derek Bolch
+ch10.py
+
+Problem: a place to play with chapter 10 material
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+class Die:
+    def __init__(self, sides):
