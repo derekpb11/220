89c16967d6627ea50f85fd86c0ced4a887aa8f00
Derek Bolch <bolchdp@g.cofc.edu>
2021-11-10 11:26:44 -0500

    hw 9

--- assignments/hw9/button.py
+++ assignments/hw9/button.py
@@ -7,3 +7,43 @@ Problem: this program will create a class for a button that will be used in thre
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
+
+from graphics import Text
+class Button:
+
+    def __init__(self, shape, label):
+        self.shape = shape
+        self.text = Text(self.shape.getCenter(), label)
+
+
+    def get_label(self):
+        return self.text.getText()
+
+
+    def draw(self, win):
+       #  button_rect = self.shape
+        self.shape.draw(win)
+        self.text.draw(win)
+
+
+    def undraw(self):
+        self.shape.undraw()
+        self.text.undraw()
+
+
+    def is_clicked(self, point):
+        rect_p1 = self.shape.getP1()
+        rect_p2 = self.shape.getP2()
+
+        if point.getX() >= rect_p1.getX() and point.getX() <= rect_p2.getX():
+            if point.getY() >= rect_p1.getY() and point.getY() <= rect_p2.getY():
+                return True
+        return False
+
+
+    def color_button(self, color):
+        self.shape.setFill(color)
+
+
+    def set_label(self, label):
+        self.text.setText(label)
--- assignments/hw9/three_door_game.py
+++ assignments/hw9/three_door_game.py
@@ -7,3 +7,68 @@ Problem: this program will create a secret door game based off of the the button
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
+from random import randint
+from graphics import GraphWin, Rectangle, Point, Text
+from button import Button
+
+
+
+def main():
+    # Create window
+    win = GraphWin("win", 600, 400)
+    # create buttons
+    but1 = Button(Rectangle(Point(100, 200), Point(175, 275)), "Door 1")
+    but1.draw(win)
+
+    but2 = Button(Rectangle(Point(250, 200), Point(325, 275)), "Door 2")
+    but2.draw(win)
+
+    but3 = Button(Rectangle(Point(400, 200), Point(475, 275)), "Door 3")
+    but3.draw(win)
+
+    # create top text
+    top_text = Text(Point(300, 100), "I have a secret door")
+    top_text.draw(win)
+
+    # create bottom text
+    bottom_text = Text(Point(300, 375), "Click to choose my door")
+    bottom_text.draw(win)
+
+    # choose door
+    my_door = randint(1, 3)
+    click = win.getMouse()
+    if but1.is_clicked(click):
+        if my_door == 1:
+            but1.color_button("green")
+            top_text.setText("You Win!")
+            bottom_text.setText("Click to close")
+        else:
+            but1.color_button("red")
+            top_text.setText("You Lost!")
+            bottom_text.setText("Door " + str(my_door) + " is my secret door")
+
+    if but2.is_clicked(click):
+        if my_door == 2:
+            but2.color_button("green")
+            top_text.setText("You Win!")
+            bottom_text.setText("Click to close")
+        else:
+            but2.color_button("red")
+            top_text.setText("You Lost!")
+            bottom_text.setText("Door " + str(my_door) + " is my secret door")
+
+    if but3.is_clicked(click):
+        if my_door == 3:
+            but1.color_button("green")
+            top_text.setText("You Win!")
+            bottom_text.setText("Click to close")
+        else:
+            but3.color_button("red")
+            top_text.setText("You Lost!")
+            bottom_text.setText("Door " + str(my_door) + " is my secret door")
+
+    win.getMouse()
+
+
+if __name__ == "__main__":
+    main()
